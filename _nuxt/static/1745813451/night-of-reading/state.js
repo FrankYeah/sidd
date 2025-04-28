ca5a94ebfaf6139bd7346eedff16cb0a92c18e88
Wendy HsinYun <wendyhsinyun@gmail.com>
2025-04-28 14:03:55 +0800

    chore: add _headers for static cache control

--- _nuxt/static/1745813451/night-of-reading/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1745813451",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnight-of-reading",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
